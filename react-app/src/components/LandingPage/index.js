2d096443364f9a244490bbc2a0f01086c110e62c
CharlesMich <charleedongo@hotmail.com>
2023-07-18 01:31:15 -0500

    landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -10,12 +10,13 @@ import "./demouser.css"
 function LandingPage() {
 
   return (
-    <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
+    // <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
+       <div className="landing-main">
               <div className="landing-sub-main">
                               {/* <p className="landing-slogan">Tell about the site</p> */}
                     <div className="landing-container">
                                
-                              <img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-logo.png" id='logo' alt='logo' />
+                              <img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo' />
                              
                               <div className="login-demo-signup-login">
                                 <div className="login-demo-signup">
