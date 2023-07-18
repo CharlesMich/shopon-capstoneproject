c832c2da27aacc968f817f88580ade814aa69198
CharlesMich <charleedongo@hotmail.com>
2023-07-18 10:44:11 -0500

    login page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -7,29 +7,32 @@ import "./landingpage.css"
 import "./demouser.css"
 
 
+// <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
+{/* <div><img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo'/></div> */}
 function LandingPage() {
 
   return (
-    // <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
-       <div className="landing-main">           
-                               
-                {/* <div><img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo'/></div> */}
-                             
-          <div className="test123">
-                <div className="login-demo-signup-login">
-                                <div className="login-demo-signup">
-                                            
-                                  <DemoUser itemText="Log in as Demo User"/>
-                                  <OpenModalButton buttonText="Sign Up" 
-                                                  style={{backgroundColor:"green"}}
-                                                  className="abc"
-                                                  modalComponent={<SignupFormModal />}
-                                                />                       
-                                </div>
-                              <LoginForm />
-                </div>                            
-          </div>     
-  </div>
+    <div className="auth-main">
+      <img className="auth-kora-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo' />
+      <p className="auth-slogan">A platform for exchanging knowledge and gaining a deeper insight into the world</p>
+      <div className="auth-container">
+        <div className="auth-container-section signupform-container">
+          <p className="auth-terms">By continuing you indicate that you agree to Koras’s Terms of Service and Privacy Policy.</p>
+          <DemoUser
+            itemText="Log in as Demo User"
+          />
+          <OpenModalButton
+            buttonText="Sign Up"
+            className="signup-btn"
+            modalComponent={<SignupFormModal />}
+          />
+        </div>
+        <div className="auth-container-section loginform-container">
+          <h1 className="login-title">Log In</h1>
+          <LoginForm />
+        </div>
+      </div>
+    </div>
   );
 }
 
