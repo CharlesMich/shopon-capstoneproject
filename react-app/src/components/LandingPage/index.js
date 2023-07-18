d7d950eef2ab2c6971353378b83b24bc599389bc
CharlesMich <charleedongo@hotmail.com>
2023-07-18 02:21:12 -0500

    landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -10,34 +10,26 @@ import "./demouser.css"
 function LandingPage() {
 
   return (
-    <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
-       {/* <div className="landing-main"> */}
-             
-                              {/* <p className="landing-slogan">Tell about the site</p> */}
-                    <div className="landing-container">
+    // <div className="landing-main" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
+       <div className="landing-main">           
                                
-                              <img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo' />
+                {/* <div><img className="landing-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo'/></div> */}
                              
-                              <div className="login-demo-signup-login">
+          <div className="test123">
+                <div className="login-demo-signup-login">
                                 <div className="login-demo-signup">
-                                              {/* <p className="landing-terms">Log in contract.</p> */}
-                                              <DemoUser
-                                                itemText="Log in as Demo User"
-                                              />
-                                             <OpenModalButton
-                                                buttonText="Sign Up" 
-                                                style={{backgroundColor:"green"}}
-                                                className="abc"
-                                                modalComponent={<SignupFormModal />}
-                                              />                       
                                             
+                                  <DemoUser itemText="Log in as Demo User"/>
+                                  <OpenModalButton buttonText="Sign Up" 
+                                                  style={{backgroundColor:"green"}}
+                                                  className="abc"
+                                                  modalComponent={<SignupFormModal />}
+                                                />                       
                                 </div>
-                                  <LoginForm />
-                                 </div>
-                              
-                    </div>
-           
-    </div>
+                              <LoginForm />
+                </div>                            
+          </div>     
+  </div>
   );
 }
 
