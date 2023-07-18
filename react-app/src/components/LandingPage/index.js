6dc21d34499845462b01f7916944e2e049685eea
CharlesMich <charleedongo@hotmail.com>
2023-07-18 10:55:37 -0500

    landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -12,12 +12,13 @@ import "./demouser.css"
 function LandingPage() {
 
   return (
+    <div className="login-container" style={{backgroundImage:`url(https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-landingpage-bg.png)`}}>
     <div className="auth-main">
       <img className="auth-kora-logo" src="https://myaaprojects.s3.us-east-2.amazonaws.com/shopon-dark-logo.png" id='logo' alt='logo' />
-      <p className="auth-slogan">A platform for exchanging knowledge and gaining a deeper insight into the world</p>
+      {/* <p className="auth-slogan">A platform for exchanging knowledge and gaining a deeper insight into the world</p> */}
       <div className="auth-container">
         <div className="auth-container-section signupform-container">
-          <p className="auth-terms">By continuing you indicate that you agree to Koras’s Terms of Service and Privacy Policy.</p>
+          {/* <p className="auth-terms">By continuing you indicate that you agree to Koras’s Terms of Service and Privacy Policy.</p> */}
           <DemoUser
             itemText="Log in as Demo User"
           />
@@ -28,10 +29,11 @@ function LandingPage() {
           />
         </div>
         <div className="auth-container-section loginform-container">
-          <h1 className="login-title">Log In</h1>
+          {/* <h1 className="login-title">Log In</h1> */}
           <LoginForm />
         </div>
       </div>
+    </div>
     </div>
   );
 }
