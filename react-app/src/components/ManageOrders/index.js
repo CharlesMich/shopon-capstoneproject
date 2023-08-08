b893a7622203efbba8c2cbc6f0cae1cb07c6bce5
CharlesMich <charleedongo@hotmail.com>
2023-08-08 10:23:15 -0500

    manage orders

--- react-app/src/components/ManageOrders/index.js
+++ react-app/src/components/ManageOrders/index.js
@@ -21,7 +21,7 @@ function ManageOrder() {
     const orderItems = useSelector(state => Object.values(state.orderItems))
     const myOrders = allOrders.filter(ele => ele.user_id === userId)
 
-    const [showItem, setShowItem] = useState(false)
+    const [showItem, setShowItem] = useState(-1)
     console.log(showItem)
     useEffect(() => {
         dispatch(fetchGetOrder())
@@ -36,45 +36,49 @@ function ManageOrder() {
         for (let i = 0; i < array.length; i++) {
             count = count + (array[i].price * array[i].quantity)
         }
-        return count;
+        return count.toFixed(2);
     }
 
-    const revealText = () => {
-        !showItem ? setShowItem(true) : setShowItem(false)
+    const revealText = (id) => {
+
+        showItem !== id? setShowItem(id): setShowItem(-1)
     }
 
     if (!sessionUser) return <Redirect to="/login" />;
 
     return (
-        <div className="manageproducts-container">
-            <div className="manageproducts-subcontainer">
+        <div className="manage-orders-container">
+            <div className="manage-orders-subcontainer">
                 {/* <h1 className ="manageproducts-h1">Manage your Orders</h1> */}
                 <h2 className="manageproducts-h2">My Orders</h2>
-                <div>{myOrders.map((ele) =>
+                <div className= "manage-order-map-container">{myOrders.map((ele, index) =>
                     <div>
-                        <div className="manageproduct-each-product">
-                            <p>order Id: {ele.id}</p>
-                            <p>Order Placed on: {(ele.created_at).split(' ').slice(1, 4).join(' ')}</p>
-                            <button onClick={revealText} >Order Details</button>
-                        </div>
-                        <div key={ele.id}>{showItem && orderItems.filter(item => item.order_id === Number(ele.id)).map(ele => (
-                                        <div>
-                                            <div className="order-map-eachitem">
+                            <div className="manage-order-each-order">
+                                        <p>order Id: {ele.id}</p>
+                                        <p>Order Placed on: {(ele.created_at).split(' ').slice(1, 4).join(' ')}</p>
+                                        <button key={ele.id} className="manage-orders-details-button" onClick={()=> revealText(index)}>Order Details</button>
+                            </div>
+                                                
+                                <div  key={ele.id} className="manageorder-orderitem-map">{showItem === index && orderItems.filter(item => item.order_id === Number(ele.id)).map(ele => (
+                                                    
+                                        <div className="order-map-eachitem-1">
                                                 <div><img style={{ width: '50px' }} src={ele.img1} alt=""></img></div>
                                                 <div>{ele.name}</div>
                                                 <div>Qty: {ele.quantity}</div>
                                                 <div>{Number(ele.price * ele.quantity).toFixed(2)}</div>
-                                            </div>
                                         </div>
-                        ))}</div>
-                        <div>Subtotal: {orderSubtotal(orderItems)}</div>
-                        <div>Tax: {orderSubtotal(orderItems) * 5 / 100}</div>
-                        <div>Total: {orderSubtotal(orderItems) + orderSubtotal(orderItems) * 5 / 100}</div>
-                    </div>
+                                ))}</div>
+                                    
+                                <div className="manage-orders-subtotal">
+                                            <div key={ele.id}>Subtotal: {orderSubtotal(orderItems.filter(item => item.order_id === Number(ele.id)))}</div>
+                                            <div key = {ele.id}>Tax: {orderSubtotal(orderItems.filter(item => item.order_id === Number(ele.id))) * 5 / 100}</div>
+                                            <div key = {ele.id}>Total: {Number(orderSubtotal(orderItems.filter(item => item.order_id === Number(ele.id)))) + (orderSubtotal(orderItems.filter(item => item.order_id === Number(ele.id))) * 5 / 100)}</div>
+                                </div>
+                    </div>            
                 )}</div>
             </div>
         </div>
-    )
+            )
 }
 
 export default ManageOrder
